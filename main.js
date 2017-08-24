47c4644736317c9e786e9f7da03da60a8e767c07
Abby Starnes <abbymstarnes@gmail.com>
2017-08-24 15:49:32 -0500

    delete

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ let depth = {
   3: '    -'
 };
 
+let masterNode = load(test);
+
 let printTree = function(node){
   if (node.children === []) {
     console.log(`${depth[node.depth]}${node.name}`);
@@ -19,7 +21,7 @@ let printTree = function(node){
   }
 };
 
-printTree(load(test));
+// printTree(load(test));
 
 let searchTree = function(node, name) {
   if (node.name === name){
@@ -38,4 +40,33 @@ let searchTree = function(node, name) {
 }
 
 let string = 'Contrary to popular belief, Lorem Ipsum is not simply random text';
-searchTree(load(test), string);
+
+let deleteNode = function(node, name){
+  if (node.name === name){
+    let parent = node.parent;
+    let children = node.children;
+    for (let t = 0; t < children.length; t++){
+      children[t].parent = parent;
+      children[t].depth = children[t].depth -1;
+    }
+    for (let y = 0; y < children.length; y++){
+      if (parent.children[y].name === name){
+        parent.children.splice(y, 1);
+        for (let x = 0; x < children.length; x++){
+          parent.children.splice(y, 0, children[x]);
+        }
+      }
+    }
+    return node;
+  }
+  if (node.children === []) {
+    return;
+  }else {
+    node.children.forEach(function(i){
+      deleteNode(i, name);
+    });
+  }
+}
+printTree(masterNode);
+deleteNode(masterNode, 'Lorem Ipsum has been the industry\'s standard dummy');
+printTree(masterNode);
