6bc5c1a202515d57cf5720d9df4588e18226446e
Narshimus <nash.williams@g.austincc.edu>
2017-08-25 09:29:38 -0500

    save and load basic functionality

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let fs = require('fs');
 let load = require('./load');
 let insert = require('./add');
+let save = require('./save');
 
 let depth = {
   0: '////////////////////////DOCUMENT OUTLINER////////////////////////',
@@ -10,10 +11,8 @@ let depth = {
   4: '      ~'
 };
 
-let tree = load(test);
-let string = 'Contrary to popular belief, Lorem Ipsum is not simply random text';
 
-let printTree = function(node){
+let printTree = function(node) {
   if (node.children === []) {
     console.log(`${depth[node.depth]}${node.name}`);
     return;
@@ -50,36 +49,48 @@ let searchTree = function(node, name) {
   }
 };
 
-fs.readFile('./sampleData.json', 'utf8', function(err,data) {
-  let tree = load(JSON.parse(data));
-  let string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
-  insert(searchTree(tree, string), '//////////Inserted string//////////');
-  printTree(tree);
+fs.readFile('./sampleData.json', 'utf8', function(err, data) {
+  if (err) {
+    console.log('could not read file');
+  } else {
+
+    let tree = load(JSON.parse(data));
+    let string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+    // insert(searchTree(tree, string), '//////////Inserted string//////////');
+    // deleteNode(searchTree(tree, '//////////Inserted string//////////'));
+    fs.writeFile('./sampleData.json', JSON.stringify(save(tree)), 'utf8', function(err, data) {
+      if (err) {
+        console.log('could not save file');
+      } else {
+        console.log('saved to disc');
+        printTree(tree);
+      }
+    });
+  }
 });
 
-let deleteNode = function(node){
-    let parent = node.parent;
-    let children = node.children;
-    if (parent){
-      if (children) {
-        for (let t = 0; t < children.length; t++){
-          children[t].parent = parent;
-          children[t].depth = children[t].depth -1;
-        }
+let deleteNode = function(node) {
+  let parent = node.parent;
+  let children = node.children;
+  if (parent) {
+    if (children) {
+      for (let t = 0; t < children.length; t++) {
+        children[t].parent = parent;
+        children[t].depth = children[t].depth - 1;
       }
-      for (let y = 0; y < parent.children.length; y++){
-        if (parent.children[y].name === node.name){
-          parent.children.splice(y, 1);
-          if (children){
-            for (let x = 0; x < children.length; x++){
-              parent.children.splice(y, 0, children[x]);
-            }
+    }
+    for (let y = 0; y < parent.children.length; y++) {
+      if (parent.children[y].name === node.name) {
+        parent.children.splice(y, 1);
+        if (children) {
+          for (let x = 0; x < children.length; x++) {
+            parent.children.splice(y, 0, children[x]);
           }
         }
       }
-      return node;
     }
-    else {
-      return null;
-    }
+    return node;
+  } else {
+    return null;
+  }
 }
--- save.js
+++ save.js
@@ -1,7 +1,19 @@
 'Use strict';
 
-let treeToObject = function(tree){
+let treeToObject = function(tree) {
   //should traverse tree and return object suitable for JSON file
+  let obj = {
+    "name": tree.name,
+    "children": []
+  };
+  if (tree.children === []) {
+    return obj;
+  } else {
+    tree.children.forEach((i)=>{
+      obj.children.push(treeToObject(i));
+    });
+    return obj;
+  }
 };
 
 module.exports = treeToObject;
